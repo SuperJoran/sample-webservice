3b70ed682b4606ac3724c4f152461191a9fbe6fa
Jorandeboever <jorandeboever@gmail.com>
2018-08-18 16:51:38 +0200

    initial commit

---
+++ src/main/java/be/superjoran/samplewebservice/SampleWebserviceApplication.java
@@ -0,0 +1,12 @@
+package be.superjoran.samplewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleWebserviceApplication.class, args);
+    }
+}
